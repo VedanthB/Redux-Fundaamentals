07ebdd6fa99b5b322c3c08865725ec8ae0baa038
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-12-31 15:53:11 +0530

    Subscribe & Binding Action Creators

--- src/index.js
+++ src/index.js
@@ -27,8 +27,48 @@ const reducer = (state = initialState, action) => {
 
 let store = createStore(reducer);
 
-console.log(store.getState());
-store.dispatch(increment());
-console.log(store.getState());
-store.dispatch(add(10));
+const subsscriber = () => console.log("subscriber!", store.getState());
+
+const unsubscribe = store.subscribe(subsscriber);
+
+unsubscribe();
+
+// if we want to put em in a neat package
+
+// const dispatchIncrement = () => store.dispatch(increment());
+// const dispatchAdd = (number) => store.dispatch(add(number));
+
+// dispatchIncrement();
+// dispatchAdd();
+
+// or
+
+// using compose
+
+// const dispatchIncrement = compose(store.dispatch, increment);
+// const dispatchAdd = compose(store.dispatch, add);
+
+// or
+
+// using compose to combine both functions
+
+// const [dispatchIncrement, dispatchAdd] = [increment, add].map((fn) =>
+//   compose(store.dispatch, fn)
+// );
+
+// using bindActionCreators
+
+const actions = bindActionCreators(
+  {
+    increment,
+    add
+  },
+  store.dispatch
+);
+
+actions.increment();
+actions.add(10);
+actions.add(100);
+actions.increment();
+
 console.log(store.getState());
