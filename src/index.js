4297776b018cb29ff7df911ee51443a413509e8e
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-12-31 18:23:57 +0530

    Log Middleware + monitor Middleware

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
   combineReducers
 } from "redux";
 
-const reducer = (state) => state;
+const reducer = (state = { count: 1 }) => state;
 
 const monitorReducerEnhancer = (createStore) => (
   reducer,
@@ -27,4 +27,50 @@ const monitorReducerEnhancer = (createStore) => (
   return createStore(monitoredReducer, initialState, enhancer);
 };
 
-const store = createStore(reducer, monitorReducerEnhancer);
+const logReducerEnhancer = (createStore) => (
+  reducer,
+  initialState,
+  enhancer
+) => {
+  const logReducer = (state, action) => {
+    console.log("old state", state, action);
+    const newState = reducer(state, action);
+    console.log("new state", newState, action);
+
+    return newState;
+  };
+
+  return createStore(logReducer, initialState, enhancer);
+};
+
+// const store = createStore(
+//   reducer,
+//   compose(logReducerEnhancer, monitorReducerEnhancer)
+// );
+
+const logMiddleware = (store) => (next) => (action) => {
+  console.log("Before", store.getState(), { action });
+  next(action);
+  console.log("After", store.getState(), { action });
+};
+
+const monitorMiddleware = (store) => (next) => (action) => {
+  const start = performance.now();
+  next(action);
+  const end = performance.now();
+  const diff = Math.round(end - start);
+
+  console.log("Reducer process time:", diff);
+};
+// const middlewares = applyMiddleware(logMiddleware);
+// const enhancer = compose(middleware, logEnhancer);
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logMiddleware, monitorMiddleware)
+);
+
+store.dispatch({ type: "cool" });
+store.dispatch({ type: "cool is cool" });
+store.dispatch({ type: "cool is cool1" });
+store.dispatch({ type: "cool is cool2" });
