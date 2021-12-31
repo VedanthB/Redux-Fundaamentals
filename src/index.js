8c24b91297736f68084cb27c039cb32642e4de94
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-12-31 17:55:04 +0530

    Enhancers monitor performance Enhancer

--- src/index.js
+++ src/index.js
@@ -6,44 +6,25 @@ import {
   combineReducers
 } from "redux";
 
-const initialState = {
-  users: [
-    { id: 1, name: "Steve" },
-    { id: 2, name: "Wes" }
-  ],
-  tasks: [
-    { title: "File the TPS reports", assignedTo: 1 },
-    { title: "Order more toner for the printer", assignedTo: null }
-  ]
+const reducer = (state) => state;
+
+const monitorReducerEnhancer = (createStore) => (
+  reducer,
+  initialState,
+  enhancer
+) => {
+  const monitoredReducer = (state, action) => {
+    const start = performance.now();
+    const newState = reducer(state, action);
+    const end = performance.now();
+    const diff = Math.round(end - start);
+
+    console.log("Reducer process time:", diff);
+
+    return newState;
+  };
+
+  return createStore(monitoredReducer, initialState, enhancer);
 };
 
-const ADD_USER = "ADD_USER";
-const ADD_TASK = "ADD_TASK";
-
-const addTask = (title) => ({ type: ADD_TASK, payload: { title } });
-const addUser = (name) => ({ type: ADD_USER, payload: { name } });
-
-const users = (state = initialState.users, action) => {
-  if (action.type === ADD_USER) {
-    return [...state, action.payload];
-  }
-
-  return state;
-};
-
-const tasks = (state = initialState.tasks, action) => {
-  if (action.type === ADD_TASK) {
-    return [...state, action.payload];
-  }
-
-  return state;
-};
-
-const reducer = combineReducers({ users, tasks });
-
-const store = createStore(reducer, initialState);
-
-store.dispatch(addUser("ved"));
-store.dispatch(addTask("vedVedved"));
-
-console.log(store.getState());
+const store = createStore(reducer, monitorReducerEnhancer);
